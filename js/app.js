afc834a97f56a17b80d0f6b9de503cdf5af2ebb0
Alex Bowen <alexbowen123@googlemail.com>
2014-03-17 14:55:22 +0000

    first commit

--- js/app.js
+++ js/app.js
@@ -2,29 +2,4 @@ var chatApp = {};
 
 $(document).ready(function () {
 
-    // cache DOM elements
-    var input = $('#chat-input'), status = $('#status');
-
-    /**
-     * Send mesage when user presses Enter key
-     */
-    input.keydown(function(e) {
-        if (e.keyCode === 13) {
-            chatApp.sendMessage($(this).val());
-        }
-    });
-
-    chatApp.connected = function () {
-        input.removeAttr('disabled');
-        status.text('Choose name:');
-    }
-
-    chatApp.sendMessage = function (msg) {
-        console.log('send message ' + msg);
-        if (!msg) {
-            return false;
-        }
-    }
-
-    //chatApp.connected();
 });
