b00de47d8b0f70992405164d337f475a98f33929
Alex Bowen <alexbowen123@googlemail.com>
2014-11-27 19:45:32 +0000

    added cucumber

--- tests/features/step_definitions/functionality.js
+++ tests/features/step_definitions/functionality.js
@@ -1,30 +1,21 @@
 module.exports = function() {
     this.When(/^the chat application is connected$/, function (callback) {
-        browser.executeScript('return window.chatApp.connected();');
-        callback();
+      // Write code here that turns the phrase above into concrete actions
+      callback.pending();
     });
 
     this.Then(/^the user is prompted for a name$/, function (callback) {
-        var expect = this.expect;
-
-        element(by.id('status')).getText().then(function(text) {
-            expect(text).to.equal('Choose name:');
-            callback();
-        });
+      // Write code here that turns the phrase above into concrete actions
+      callback.pending();
     });
 
     this.When(/^the user enters a name$/, function (callback) {
-        element(by.id('chat-input')).sendKeys('Alex');
-        element(by.id('chat-input')).sendKeys(protractor.Key.ENTER);
-        callback();
+      // Write code here that turns the phrase above into concrete actions
+      callback.pending();
     });
 
     this.Then(/^the user name appears as connected$/, function (callback) {
-        var expect = this.expect;
-
-        element(by.id('status')).getText().then(function(text) {
-            expect(text).to.equal('Alex: connected');
-            callback();
-        });
+      // Write code here that turns the phrase above into concrete actions
+      callback.pending();
     });
 };
