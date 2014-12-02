7df9b6403f798835c4865ffa559f2d305365d488
Alex Bowen <alexbowen123@googlemail.com>
2014-12-02 09:15:08 +0000

    fixed tests as some werent async

--- tests/features/step_definitions/global.js
+++ tests/features/step_definitions/global.js
@@ -1,6 +1,5 @@
 module.exports = function () {
-  this.Given(/^I go to the web application$/, function (callback) {
-    this.ptor.get('http://localhost:8000');
-    callback();
-  });
+	this.Given(/^I go to the web application$/, function (callback) {
+	    this.ptor.get('http://localhost:8000').then(callback);
+	});
 }
