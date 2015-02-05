f2eba0ff14acf5f84d59cf199b6ef8b9b8b869df
Alex Bowen <alexbowen123@googlemail.com>
2015-02-05 11:30:05 +0000

    added step defs

--- tests/features/step_definitions/structure.js
+++ tests/features/step_definitions/structure.js
@@ -1,3 +1,26 @@
 module.exports = function() {
+  this.Given(/^I go to the web application$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+  
+  this.Then(/^i see a header section$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
 
+  this.Then(/^i see a profile section$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
+  this.Then(/^i see a chat section$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
+
+  this.Then(/^i see a footer section$/, function (callback) {
+    // Write code here that turns the phrase above into concrete actions
+    callback.pending();
+  });
 }
